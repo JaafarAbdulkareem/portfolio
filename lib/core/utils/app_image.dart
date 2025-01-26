fd780d42b16e599fb2c56d6bab537999fb2cf6ec
King <jafaar.abdulkareem@gmail.com>
2025-01-26 07:12:05 +0530

    add image

---
+++ lib/core/utils/app_image.dart
@@ -0,0 +1,24 @@
+class AppImage {
+  AppImage._();
+  static const String _path = 'assets/images';
+  static const String imagesArduino = "$_path/arduino.svg";
+  static const String imagesCertificate = "$_path/certificate.svg";
+  static const String imagesCss = "$_path/css.svg";
+  static const String imagesDart = "$_path/dart.svg";
+  static const String imagesFirebase = "$_path/firebase.svg";
+  static const String imagesFlutter = "$_path/flutter.svg";
+  static const String imagesGit = "$_path/git.svg";
+  static const String imagesGithub = "$_path/github.svg";
+  static const String imagesHtml = "$_path/html.svg";
+  static const String imagesJava = "$_path/java.svg";
+  static const String imagesLinkedin = "$_path/linkedin.svg";
+  static const String imagesMysql = "$_path/mysql.svg";
+  static const String imagesPhp = "$_path/php.svg";
+  static const String imagesPortfolio1 = "$_path/portfolio1.svg";
+  static const String imagesPortfolio2 = "$_path/portfolio2.svg";
+  static const String imagesPostman = "$_path/postman.svg";
+  static const String imagesProgrammer = "$_path/programmer.svg";
+  static const String imagesPython = "$_path/python.svg";
+  static const String imagesResponsive = "$_path/responsive.svg";
+  static const String imagesWhatsapp = "$_path/whatsapp.svg";
+}
