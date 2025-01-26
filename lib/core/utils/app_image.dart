c18a9244d9495a8cedd49c38e9dca8ba9db451d8
King <jafaar.abdulkareem@gmail.com>
2025-01-27 00:47:03 +0530

    insert new image for mail address for introduction

--- lib/core/utils/app_image.dart
+++ lib/core/utils/app_image.dart
@@ -1,24 +1,101 @@
+// ignore_for_file: prefer_single_quotes
 class AppImage {
   AppImage._();
-  static const String _path = 'assets/images';
-  static const String imagesArduino = "$_path/arduino.svg";
-  static const String imagesCertificate = "$_path/certificate.svg";
-  static const String imagesCss = "$_path/css.svg";
-  static const String imagesDart = "$_path/dart.svg";
-  static const String imagesFirebase = "$_path/firebase.svg";
-  static const String imagesFlutter = "$_path/flutter.svg";
-  static const String imagesGit = "$_path/git.svg";
-  static const String imagesGithub = "$_path/github.svg";
-  static const String imagesHtml = "$_path/html.svg";
-  static const String imagesJava = "$_path/java.svg";
-  static const String imagesLinkedin = "$_path/linkedin.svg";
-  static const String imagesMysql = "$_path/mysql.svg";
-  static const String imagesPhp = "$_path/php.svg";
-  static const String imagesPortfolio1 = "$_path/portfolio1.svg";
-  static const String imagesPortfolio2 = "$_path/portfolio2.svg";
-  static const String imagesPostman = "$_path/postman.svg";
-  static const String imagesProgrammer = "$_path/programmer.svg";
-  static const String imagesPython = "$_path/python.svg";
-  static const String imagesResponsive = "$_path/responsive.svg";
-  static const String imagesWhatsapp = "$_path/whatsapp.svg";
+  
+  /// Assets for imagesArduino
+  /// assets/images/arduino.svg
+  static const String imagesArduino = "assets/images/arduino.svg";
+
+  /// Assets for imagesBriefcase
+  /// assets/images/briefcase.svg
+  static const String imagesBriefcase = "assets/images/briefcase.svg";
+
+  /// Assets for imagesCertificate
+  /// assets/images/certificate.svg
+  static const String imagesCertificate = "assets/images/certificate.svg";
+
+  /// Assets for imagesCss
+  /// assets/images/css.svg
+  static const String imagesCss = "assets/images/css.svg";
+
+  /// Assets for imagesDart
+  /// assets/images/dart.svg
+  static const String imagesDart = "assets/images/dart.svg";
+
+  /// Assets for imagesDownload
+  /// assets/images/download.svg
+  static const String imagesDownload = "assets/images/download.svg";
+
+  /// Assets for imagesFirebase
+  /// assets/images/firebase.svg
+  static const String imagesFirebase = "assets/images/firebase.svg";
+
+  /// Assets for imagesFlutter
+  /// assets/images/flutter.svg
+  static const String imagesFlutter = "assets/images/flutter.svg";
+
+  /// Assets for imagesGit
+  /// assets/images/git.svg
+  static const String imagesGit = "assets/images/git.svg";
+
+  /// Assets for imagesGithub
+  /// assets/images/github.svg
+  static const String imagesGithub = "assets/images/github.svg";
+
+  /// Assets for imagesHtml
+  /// assets/images/html.svg
+  static const String imagesHtml = "assets/images/html.svg";
+
+  /// Assets for imagesJava
+  /// assets/images/java.svg
+  static const String imagesJava = "assets/images/java.svg";
+
+  /// Assets for imagesLinkedin
+  /// assets/images/linkedin.svg
+  static const String imagesLinkedin = "assets/images/linkedin.svg";
+
+  /// Assets for imagesMail
+  /// assets/images/mail.svg
+  static const String imagesMail = "assets/images/mail.svg";
+
+  /// Assets for imagesMap
+  /// assets/images/map.svg
+  static const String imagesMap = "assets/images/map.svg";
+
+  /// Assets for imagesMysql
+  /// assets/images/mysql.svg
+  static const String imagesMysql = "assets/images/mysql.svg";
+
+  /// Assets for imagesPhp
+  /// assets/images/php.svg
+  static const String imagesPhp = "assets/images/php.svg";
+
+  /// Assets for imagesPortfolio1
+  /// assets/images/portfolio1.svg
+  static const String imagesPortfolio1 = "assets/images/portfolio1.svg";
+
+  /// Assets for imagesPortfolio2
+  /// assets/images/portfolio2.svg
+  static const String imagesPortfolio2 = "assets/images/portfolio2.svg";
+
+  /// Assets for imagesPostman
+  /// assets/images/postman.svg
+  static const String imagesPostman = "assets/images/postman.svg";
+
+  /// Assets for imagesProgrammer
+  /// assets/images/programmer.svg
+  static const String imagesProgrammer = "assets/images/programmer.svg";
+
+  /// Assets for imagesPython
+  /// assets/images/python.svg
+  static const String imagesPython = "assets/images/python.svg";
+
+  /// Assets for imagesResponsive
+  /// assets/images/responsive.svg
+  static const String imagesResponsive = "assets/images/responsive.svg";
+
+  /// Assets for imagesWhatsapp
+  /// assets/images/whatsapp.svg
+  static const String imagesWhatsapp = "assets/images/whatsapp.svg";
 }
+
