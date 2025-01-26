1e36f2a21b4b723612da36972342657054cbfc28
King <jafaar.abdulkareem@gmail.com>
2025-01-26 11:13:33 +0530

    add app style for program and constant scale for desktop

---
+++ lib/core/utils/app_style.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+import 'package:my_portfolio/core/utils/constant_scale.dart';
+
+double getResponsiveFontSize(BuildContext context, {required double fontSize}) {
+  double scaleFactor = getScaleFactor(context);
+  double responsiveFontSize = fontSize * scaleFactor;
+  double lowerLimit = fontSize * 0.8;
+  double upperLimit = fontSize * 1.2;
+  return responsiveFontSize.clamp(lowerLimit, upperLimit);
+}
+
+double getScaleFactor(BuildContext context) {
+  double width = MediaQuery.sizeOf(context).width;
+  if (width < ConstantScale.tablet) {
+    return width / 700;
+  } else if (width < ConstantScale.desktop) {
+    return width / 1000;
+  } else {
+    return width / 1800;
+  }
+}
+
+abstract class AppStyles {
+  AppStyles._();
+  static TextStyle styleIBMRegular14(context) {
+    return TextStyle(
+      color: Colors.white,
+      fontSize: getResponsiveFontSize(context, fontSize: 14),
+      fontFamily: 'IBM Plex Mono',
+      fontWeight: FontWeight.w400,
+    );
+  }
+
+  static TextStyle styleIBMRegular24(context) {
+    return TextStyle(
+      color: const Color(0xFF292F36),
+      fontSize: getResponsiveFontSize(context, fontSize: 24),
+      fontFamily: 'IBM Plex Mono',
+      fontWeight: FontWeight.w400,
+    );
+  }
+
+  static TextStyle styleIBMMedium48(context) {
+    return TextStyle(
+      color: const Color(0xFF12F7D6),
+      fontSize: getResponsiveFontSize(context, fontSize: 48),
+      fontFamily: 'IBM Plex Mono',
+      fontWeight: FontWeight.w500,
+    );
+  }
+
+  static TextStyle styleIBMRegular16(context) {
+    return TextStyle(
+      color: Colors.white,
+      fontSize: getResponsiveFontSize(context, fontSize: 16),
+      fontFamily: 'IBM Plex Mono',
+      fontWeight: FontWeight.w400,
+    );
+  }
+
+  static TextStyle styleIBMMedium32(context) {
+    return TextStyle(
+      color: Colors.white,
+      fontSize: getResponsiveFontSize(context, fontSize: 32),
+      fontFamily: 'IBM Plex Mono',
+      fontWeight: FontWeight.w500,
+    );
+  }
+
+  static TextStyle styleUbuntuRegular20(context) {
+    return TextStyle(
+      color: const Color(0xFF292F36),
+      fontSize: getResponsiveFontSize(context, fontSize: 20),
+      fontFamily: 'Ubuntu',
+      fontWeight: FontWeight.w400,
+    );
+  }
+
+  static TextStyle styleUbuntuRegular32(context) {
+    return TextStyle(
+      color: Colors.white,
+      fontSize: getResponsiveFontSize(context, fontSize: 32),
+      fontFamily: 'Ubuntu',
+      fontWeight: FontWeight.w400,
+    );
+  }
+
+  static TextStyle styleUbuntuLight16(context) {
+    return TextStyle(
+      color: const Color(0xFF12F7D6),
+      fontSize: getResponsiveFontSize(context, fontSize: 16),
+      fontFamily: 'Ubuntu',
+      fontWeight: FontWeight.w300,
+    );
+  }
+
+  static TextStyle styleUbuntuLite16(context) {
+    return TextStyle(
+      color: const Color(0xFF12F7D6),
+      fontSize: getResponsiveFontSize(context, fontSize: 16),
+      fontFamily: 'Ubuntu',
+      fontWeight: FontWeight.w300,
+    );
+  }
+
+  static TextStyle styleUbuntuRegular45(context) {
+    return TextStyle(
+      color: Colors.white,
+      fontSize: getResponsiveFontSize(context, fontSize: 45),
+      fontFamily: 'Ubuntu',
+      fontWeight: FontWeight.w400,
+    );
+  }
+}
---
+++ lib/core/utils/constant_scale.dart
@@ -0,0 +1,17 @@
+// import 'package:flutter/material.dart';
+
+class ConstantScale {
+  static double tablet = 855;
+  static double desktop = 1185;
+
+  // static double calculateResponsiveSize(
+  //   BuildContext context, {
+  //   required double minSize,
+  //   required double maxSize,
+  //   required double multiplier,
+  // }) {
+  //   double screenWidth = MediaQuery.sizeOf(context).width;
+  //   double calculatedSize = screenWidth * multiplier;
+  //   return calculatedSize.clamp(minSize, maxSize);
+  // }
+}
