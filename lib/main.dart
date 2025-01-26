723aa614808d26f9a248d468690f1f07b3b7ec87
King <jafaar.abdulkareem@gmail.com>
2025-01-26 07:24:16 +0530

    create home page and screen page also leng , fonts folder

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:my_portfolio/screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,6 +13,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Portfolio',
       theme: ThemeData(),
+      home: const Screen(),
     );
   }
 }
