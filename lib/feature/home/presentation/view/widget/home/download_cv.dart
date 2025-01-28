3477af882e090d5841158b1da800fde3ee383feb
King <jafaar.abdulkareem@gmail.com>
2025-01-28 11:22:28 +0530

    create button for download cv button

--- lib/feature/home/presentation/view/widget/home/download_cv.dart
+++ lib/feature/home/presentation/view/widget/home/download_cv.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
+import 'package:my_portfolio/core/animation_widget/fill_liquid_svg_button_widget.dart';
 import 'package:my_portfolio/core/controller/language/extension_language.dart';
 import 'package:my_portfolio/core/utils/app_color.dart';
 import 'package:my_portfolio/core/utils/app_image.dart';
 import 'package:my_portfolio/core/utils/app_style.dart';
+import 'package:my_portfolio/core/utils/constant_file.dart';
 import 'package:my_portfolio/core/utils/file_utils.dart';
 import 'package:my_portfolio/core/utils/key_language.dart';
-import 'package:my_portfolio/core/widget/animated_button.dart';
+import 'package:my_portfolio/core/animation_widget/animated_button.dart';
 
 class DownloadCv extends StatelessWidget {
   const DownloadCv({
@@ -15,55 +16,41 @@ class DownloadCv extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return AnimatedButton(
-      child: (isHaver) => Container(
-        padding: const EdgeInsets.symmetric(
-          horizontal: 32,
-          vertical: 16,
-        ),
-        decoration: ShapeDecoration(
-          color: AppColor.secondard,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(32),
-          ),
-        ),
-        child: GestureDetector(
-          onTap: () async {
-            try {
-              await FileUtils.downloadAssetFile(
-                'assets/file/cv.pdf', // Path to the PDF file in assets
-                'flutter_CV.pdf', // Desired name of the file in storage
-              );
-              if (!context.mounted) return;
-              ScaffoldMessenger.of(context).showSnackBar(
-                SnackBar(
-                  content: Text(
-                    KeyLanguage.successfully.tr(context),
-                  ),
-                ),
-              );
-            } catch (e) {
-              ScaffoldMessenger.of(context).showSnackBar(
-                SnackBar(
-                  content: Text(
-                    '${KeyLanguage.successfully.tr(context)}: $e',
-                  ),
-                ),
-              );
-            }
-          },
-          child: Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Text(
-                KeyLanguage.download.tr(context),
-                style: AppStyles.styleUbuntuRegular20(context),
-              ),
-              const SizedBox(width: 16),
-              SvgPicture.asset(AppImage.imagesDownload)
-            ],
-          ),
-        ),
+      child: (isHaver) => FillLiquidSvgButtonWidget(
+        mainColor: AppColor.secondard,
+        fillColor: AppColor.primary,
+        text: KeyLanguage.download,
+        style: AppStyles.styleUbuntuRegular20(context),
+        image: AppImage.imagesDownload,
+        onPressed: () {
+          downloadCV(context);
+        },
       ),
     );
+  }
+
+  void downloadCV(BuildContext context) async {
+    try {
+      await FileUtils.downloadAssetFile(
+        ConstantFile.pathFileCV,
+        ConstantFile.newName,
+      );
+      if (!context.mounted) return;
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(
+            KeyLanguage.successfully.tr(context),
+          ),
+        ),
+      );
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(
+            '${KeyLanguage.successfully.tr(context)}: $e',
+          ),
+        ),
+      );
+    }
   }
 }
